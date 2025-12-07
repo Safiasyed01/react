618f1edce30379aca94c498e6476016de158da52
YOUR_GITHUB_USERNAME <YOUR_EMAIL>
2025-12-07 18:53:04 +0500

    fetch apis

--- local-storage/src/App.jsx
+++ local-storage/src/App.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
-
+ 
 const App = () => {
+ const getData = async()=> {
+ const response =   await fetch('https://jsonplaceholder.typicode.com/todos')
+ const data = await response.json()  
+ console.log(data)
+  }
   // localStorage.clear()
   // sessionStorage.clear()
   // localStorage.setItem('user', 'safia')
   // const user = localStorage.getItem('user')
   // console.log(user)
-  // localStorage.removeItem('user')
-  const user ={
-    userName:'safia',
-    age:84,
-    city:'pesh'
-  }
-  localStorage.setItem('user',JSON.stringify(user))
+  // // localStorage.removeItem('user')
+  // const user ={
+  //   userName:'safia',
+  //   age:84,
+  //   city:'pesh'
+  // }
+  // localStorage.setItem('user',JSON.stringify(user))
+
+
   return (
     <div>
-      local
+      <button onClick={getData}>getData</button>
     </div>
   )
 }
